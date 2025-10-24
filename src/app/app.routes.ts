bebe6aec735ee30993c3c8716076b270092c24ac
marcorapg <marcorapg@gmail.com>
2025-10-24 02:11:56 -0300

    Primeira versão

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Autoservico } from './pages/autoservico/autoservico';
 import { Criarsolicitacao } from './pages/autoservico/criarsolicitacao/criarsolicitacao';
+import { Login } from './pages/login/login';
 
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'autoservico', component: Autoservico },
   { path: 'autoservico/criarsolicitacao', component: Criarsolicitacao },
+  { path: 'login', component: Login },
   { path: '**', redirectTo: '' } // optional fallback
 ];
